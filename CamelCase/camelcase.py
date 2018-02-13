60acc767a75ba23b0bc6c6c6ce7611196325af45
Jack <jackxthexrabbit@gmail.com>
2018-02-13 02:06:38 -0600

    Testing for camelcase was completed.

--- CamelCase/camelcase.py
+++ CamelCase/camelcase.py
@@ -1,3 +1,7 @@
+def main():
+    print(camelcase(input("Enter a sentence and I will convert it to camelCase.\n")))
+
+
 def camelcase(string):
     # separate by word
     # found .title @stackoverflow while looking for way to make all lowercase
@@ -7,3 +11,8 @@ def camelcase(string):
     # join words into single word
     new_name = ''.join(word_list)
     return new_name
+
+
+if __name__ == "__main__":
+    main()
+
--- tests/test_camelcase.py
+++ tests/test_camelcase.py
@@ -1,4 +1,21 @@
 import unittest
 from CamelCase import camelcase
 
-def camel_case(sentence):
+
+class TestCamelCase(unittest.TestCase):
+
+    def test_camel_case_case(self):
+
+        self.assertEqual('helloWorld', camelcase.camelcase("Hello World"))
+
+        self.assertEqual('helloWorld', camelcase.camelcase("HeLlO WoRlD"))
+
+        self.assertEqual('helloWorld', camelcase.camelcase("HELLO World"))
+
+    def test_camel_case_spaces(self):
+
+        self.assertEqual('helloWorld', camelcase.camelcase("HelloWorld"))
+
+        self.assertEqual('helloWorldHowAreYou', camelcase.camelcase("Hello world how are you"))
+
+        self.assertEqual('hellOWOrld', camelcase.camelcase("Hell o W orld"))
